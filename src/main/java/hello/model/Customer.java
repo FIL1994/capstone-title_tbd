9ea6983a6bd90ccd25c20be3ed222587e37e6e43
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-03-30 13:50:38 -0400

    update customer

--- src/main/java/hello/model/Customer.java
+++ src/main/java/hello/model/Customer.java
@@ -11,6 +11,12 @@ public class Customer extends Person {
     private String cellPhoneNumber;
     private String workPhoneNumber;
     private String companyName;
+    private String primaryContactName;
+    private String secondaryContactName;
+    private String primaryEmail;
+    private String secondaryEmail;
+    private String primaryPhoneNumber;
+    private String secondaryPhoneNumber;
 
     @JsonIgnore
     @OneToMany(mappedBy = "customer")
@@ -38,6 +44,54 @@ public class Customer extends Person {
 
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
+    }
+
+    public String getPrimaryContactName() {
+        return primaryContactName;
+    }
+
+    public void setPrimaryContactName(String primaryContactName) {
+        this.primaryContactName = primaryContactName;
+    }
+
+    public String getSecondaryContactName() {
+        return secondaryContactName;
+    }
+
+    public void setSecondaryContactName(String secondaryContactName) {
+        this.secondaryContactName = secondaryContactName;
+    }
+
+    public String getPrimaryEmail() {
+        return primaryEmail;
+    }
+
+    public void setPrimaryEmail(String primaryEmail) {
+        this.primaryEmail = primaryEmail;
+    }
+
+    public String getSecondaryEmail() {
+        return secondaryEmail;
+    }
+
+    public void setSecondaryEmail(String secondaryEmail) {
+        this.secondaryEmail = secondaryEmail;
+    }
+
+    public String getPrimaryPhoneNumber() {
+        return primaryPhoneNumber;
+    }
+
+    public void setPrimaryPhoneNumber(String primaryPhoneNumber) {
+        this.primaryPhoneNumber = primaryPhoneNumber;
+    }
+
+    public String getSecondaryPhoneNumber() {
+        return secondaryPhoneNumber;
+    }
+
+    public void setSecondaryPhoneNumber(String secondaryPhoneNumber) {
+        this.secondaryPhoneNumber = secondaryPhoneNumber;
     }
 
     public Set<Project> getProjects() {
